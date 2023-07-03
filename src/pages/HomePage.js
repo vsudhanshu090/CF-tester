72eb788d5342015ba10770fe6423928e9ffc93ad
Sudhanshu <vsudhanshu090@gmail.com>
2023-07-04 02:09:38 +0530

    home page frontend

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid";
+import "./HomePageStyles.css";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -14,51 +15,73 @@ function HomePage() {
     setRoomId(id);
     toast.success("New room created");
   }
+
   function handleRoomIdChange(e) {
     setRoomId(e.target.value);
   }
+
   function handleUsernameChange(e) {
     setUsername(e.target.value);
   }
+
   function navigateToEditorPage() {
     if (!roomId || !username) {
-      toast.error("room id and username required");
+      toast.error("Room ID and username are required");
       return;
     }
     navigate(`/editor/${roomId}`, { state: { roomId, username } });
   }
 
   return (
-    <div>
-      <h1>hello yay</h1>
+    <div className="container">
+      <div className="grid-container">
+        <div className="left-side">
+          <h1 className="title">CoWrite IDE</h1>
+          <h3 className="description">Your friendly real-time code editor</h3>
+        </div>
+        <div className="separator"></div> {/* Add separator */}
+        <div className="right-side">
+          <form>
+            <div className="form-row">
+              <input
+                class="inputArea"
+                type="text"
+                placeholder="Room ID"
+                value={roomId}
+                onChange={handleRoomIdChange}
+                required
+              />
+            </div>
 
-      <form>
-        <label for="room-id">Room ID:</label>
-        <input
-          type="text"
-          id="room-id"
-          name="room-id"
-          value={roomId}
-          onChange={handleRoomIdChange}
-          required
-        />
+            <div className="form-row">
+              <input
+                class="inputArea"
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+              />
+            </div>
 
-        <label for="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={handleUsernameChange}
-          required
-        />
+            <div className="button-container">
+              <button className="join-button" onClick={navigateToEditorPage}>
+                Join
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
 
-        <button onClick={navigateToEditorPage}>join</button>
-      </form>
-
-      <a href="" onClick={createNewRoom}>
-        create new room
-      </a>
+      <div className="collaborate-container">
+        <p className="collaborate-text">
+          Don't have a room? <br />
+          Start Collaborating and Experience Seamless Real-Time Code Editing
+        </p>
+        <a className="create-room-link" href="#" onClick={createNewRoom}>
+          Create a Room!
+        </a>
+      </div>
     </div>
   );
 }
